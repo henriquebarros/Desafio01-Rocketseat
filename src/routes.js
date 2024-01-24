adb4c4a69b43093e2b1e49f916404c5018a986cf
henriquebarros <henriquebarros@live.com>
2024-01-24 15:45:49 -0300

    Ainda com aplicação em Stateful - testantado os métodos PUT e DELETE

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,29 @@
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js';
 
 
 const tasks = [];
 
 export const routes = [
+    
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req,res) => {
-
-            return res.setHeader('Content-type','application/json').end(JSON.stringify(tasks))
+            
+            return res.end(JSON.stringify(tasks))
         }
     },
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req,res) => {
+            const { title, description } = req.body;
+
             tasks.push({
                 id:randomUUID(),
-                title: 'Entendendo Streams no Node',
-                description: 'Nessa aula prática vamos conhecer o conteito de streams dentro do Node, mostrando explempos de como essa funcionalidade pode ajudar no dia a dia em nossas aplicações.',
+                title,
+                description,
                 completed_at: null,
                 created_at: new Date(),
                 updated_at: new Date(),
@@ -27,7 +31,41 @@ export const routes = [
     
             return res.writeHead(201).end();
         }
-    }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req,res) => {
+            const { ...data } =  req.body;
+
+            const { id } = req.params;
+            const findTask = tasks.findIndex(task=>task.id===id)
+
+            if(findTask===-1){
+                return res.writeHead(404).end(JSON.stringify({error:'Register not found'}));
+            }
+
+            Object.assign(tasks[findTask], { ...data, updated_at: new Date() })
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req,res) => {
+            const { id } = req.params;
+
+            const findTask = tasks.findIndex(task=>task.id===id)
+            if(findTask===-1){
+                return res.writeHead(404).end(JSON.stringify({error:'Register not found'}));
+            }
+
+            tasks.splice(findTask,1)
+
+            return res.writeHead(204).end()
+        }
+    },
 
 
 ]
--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
 import http from 'node:http'
 import { routes } from './routes.js';
+import { json } from './middlewares/json.js';
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async(req,res)=>{
     const { method, url } = req;
+    
+    await json(req,res)
+
+
 
     const route = routes.find(route=> {
-        return route.method === method && route.path===url
+        return route.method === method && route.path.test(url)
     })
 
     if(route){
-        return route.handler(req, res)
+        //   /^\/tasks\/(?<id>[a-z0-9-_]+)/
+        // url /tasks/dd20f404-0dc4-4bbb-bb8c-df67f56be681
+
+        
+      const routeParams = url.match(route.path)
+      
+      if(routeParams.groups){
+        const { ...params } = routeParams.groups;
+
+        req.params = params
+      }
+      
+
+      return route.handler(req, res)
     }
     
 
